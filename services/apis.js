4b584d62aeefb638fe16fed7230536bb72d49e2e
Mohit Kumar Mandal <106464018+mohit1721@users.noreply.github.com>
2025-01-20 15:42:28 +0530

    Update apis.js

--- services/apis.js
+++ services/apis.js
@@ -2,9 +2,9 @@
 // process.env.REACT_APP_BASE_URL;
 // const BASE_URL="http://localhost:5000/api/v1" // ||
 export const authENDPOINTS={
-    SIGNUP_API: "https://interactive-data-table-application.onrender.com/api/v1" + "/auth/register",
-    LOGIN_API: "https://interactive-data-table-application.onrender.com/api/v1" + "/auth/login",
+    SIGNUP_API: "https://interactive-data-table-application.onrender.com/api/v1/auth/register",
+    LOGIN_API: "https://interactive-data-table-application.onrender.com/api/v1/auth/login",
 }
 export const dataENDPOINTS={
-    GET_DATA_API: "https://interactive-data-table-application.onrender.com/api/v1"+"/data"
+    GET_DATA_API: "https://interactive-data-table-application.onrender.com/api/v1/data"
 }
