17379ba12ce61e398ca277cc93e5d8bdb59698c1
mohit1721 <mohitmandal192@gmail.com>
2025-01-20 15:11:07 +0530

    endpint

--- services/apis.js
+++ services/apis.js
@@ -1,5 +1,5 @@
-// const BASE_URL= "https://interactive-data-table-application.onrender.com/api/v1" || process.env.REACT_APP_BASE_URL;
-const BASE_URL="http://localhost:5000/api/v1"
+const BASE_URL= "https://interactive-data-table-application.onrender.com/api/v1" || process.env.REACT_APP_BASE_URL;
+// const BASE_URL="http://localhost:5000/api/v1"
 export const authENDPOINTS={
     SIGNUP_API: BASE_URL + "/auth/register",
     LOGIN_API: BASE_URL + "/auth/login",
